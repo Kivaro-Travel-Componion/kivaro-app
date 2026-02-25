0bc952fa87b9f76da80e96c7e519a8431b104073
Nguyen Trong Hieu <hieu745233@gmail.com>
2026-02-25 21:17:50 +0700

    mene-navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,84 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import React from 'react'
+import { Tabs } from 'expo-router'
+import { Image, View } from 'react-native'
+import { icons } from '@/constants/icons'
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+const TabIcon = ({ icon, focused }: TabIconProp) => {
+    return (
+        <View>
+            <View>
+                <Image source={icon} resizeMode='contain'  style={[{ tintColor: focused ? '#2B8CEE' : '#94A3B8' }]}/>
+            </View>
+        </View>
+    );
+}
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+        screenOptions={{
+            tabBarItemStyle: {
+                width: '100%',
+                height: '100%',
+                marginTop: 8
+            },
+            tabBarStyle: {
+                borderRadius: 50,
+                marginHorizontal: 10,
+                marginBottom: 40,
+                height: 74,
+                position: 'absolute',
+                overflow: 'hidden',
+                borderWidth: 1
+            },
+            tabBarLabelStyle: {
+                fontSize: 11,
+                fontWeight: 'bold',
+            },
+            tabBarActiveTintColor: '#2B8CEE',
+            tabBarInactiveTintColor: '#94A3B8',
         }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+    >
+        <Tabs.Screen 
+            name='index'
+            options={{
+                title: 'Home', 
+                headerShown: false,
+                tabBarIcon: ({ focused }) => (
+                    <TabIcon icon={icons.home} focused={focused} />
+                )
+            }}
+        />
+        <Tabs.Screen 
+            name='explore'
+            options={{
+                title: 'Explore', 
+                headerShown: false,
+                 tabBarIcon: ({ focused }) => (
+                    <TabIcon icon={icons.explore} focused={focused} />
+                )
+            }}
+        />
+        <Tabs.Screen 
+            name='trips'
+            options={{
+                title: 'Trips', 
+                headerShown: false,
+                tabBarIcon: ({ focused }) => (
+                    <TabIcon icon={icons.trips} focused={focused} />
+                )
+            }}
+        />
+        <Tabs.Screen 
+            name='profile'
+            options={{
+                title: 'Profile', 
+                headerShown: false,
+                tabBarIcon: ({ focused }) => (
+                    <TabIcon icon={icons.profile} focused={focused}/>
+                )
+            }}
+        />
     </Tabs>
-  );
+  )
 }
---
+++ constants/icons.ts
@@ -0,0 +1,11 @@
+import home from "@/assets/icons/home-icon.png";
+import explore from "@/assets/icons/explore-icon.png";
+import trips from "@/assets/icons/trips-icon.png";
+import profile from "@/assets/icons/profile-icon.png";
+
+export const icons = {
+    home,
+    explore,
+    trips,
+    profile
+};
